dbed8fd7856569c5b66be149a5b57c3894ea43b2
AdRiley <riley.ad@gmail.com>
2016-06-01 04:25:34 -0600

    Clean up macros folder

--- Macros/AssistingMacros/powershell.ps1
+++
@@ -1,7 +0,0 @@
-param(
-[String]$step,
-[String]$op
-)
-Add-Type -A System.IO.Compression.FileSystem
-[IO.Compression.ZipFile]::CreateFromDirectory($op+'\temp\', $op+'\'+$step+'.twbx')
-Get-ChildItem -Path $op'\temp\' -Include *.* -File -Recurse | foreach { $_.Delete()}
